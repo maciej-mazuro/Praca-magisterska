25fdc1b673a6596e7c9e90dd9fab672d18ec8a51
Maciej Mazuro <maciej.mazuro@student.wat.edu.pl>
2021-07-01 13:12:21 +0200

    fixed path bug

--- watermark_with_image.py
+++ watermark_with_image.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import os
 from utils import CFEVideoConf, image_resize
 
 cap = cv2.VideoCapture(0)
@@ -9,8 +10,11 @@ frames_per_seconds = 24
 config = CFEVideoConf(cap, filepath=save_path, res='720p') 
 out = cv2.VideoWriter(save_path, config.video_type, frames_per_seconds, config.dims)
 
-img_path = "C:\\Users\\Maciek\\projects\\Praca-magisterska\\images\\watka.png" #komputer
-#img_path = "C:\\Users\\ADMIN\\projects\\Praca-magisterska\\images\\watka.jpg" #laptop
+user_name = os.getenv("USERNAME")
+if user_name == 'Maciek':
+	img_path = "C:\\Users\\Maciek\\projects\\Praca-magisterska\\images\\watka.png"
+else:
+	img_path = "C:\\Users\\ADMIN\\projects\\Praca-magisterska\\images\\watka.jpg"
 
 logo = cv2.imread(img_path, -1)
 watermark = image_resize(logo, height=100)
